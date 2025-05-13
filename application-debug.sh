cec37dee3dc976cf70448979795b19eab877f336
Arthur de Araújo Farias <adaraujo@niteurope.com>
2025-05-13 17:05:57 +0200

    Modified compilation scripts.

--- application-debug.sh
+++ application-debug.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash -e
 
-ssh -t root@$1 'CONFIGURATION_DIRECTORY=/home/clamir/conf gdbserver 0.0.0.0:2000 /usr/local/bin/clamir-linux-server-ctrl-tcp'
+ssh -t root@$1 'CONFIGURATION_DIRECTORY=/home/clamir/conf gdbserver 0.0.0.0:2001 /usr/local/bin/clamir-linux-server-ctrl-tcp'
